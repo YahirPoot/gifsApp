5a2d5a72ad74c690006dc375eb3fc60cdabe5807
Angel Yahir Poot Ucan <135137591+YahirPoot@users.noreply.github.com>
2024-08-20 19:02:50 -0600

    creación de un componente para el uso de un svg de carga  en los gifs

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html'
+})
+export class LazyImageComponent implements OnInit{
+
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = '';
+
+  public loaded: boolean = false;
+
+  ngOnInit(): void {
+    if (!this.url) throw new Error('URL is required.');
+  }
+
+  onLoad() {
+    console.log('Image was loaded 😎');
+    this.loaded = true;
+  }
+
+}
