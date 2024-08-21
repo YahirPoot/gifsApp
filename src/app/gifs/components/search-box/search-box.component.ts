a94a3aa6d62b04c11f67401d3879b03bca38630b
Angel Yahir Poot Ucan <135137591+YahirPoot@users.noreply.github.com>
2024-08-20 21:29:29 -0600

    actualizacion del diseño de la pagina

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -4,12 +4,17 @@ import { GifsService } from '../../services/gifs.service';
 @Component({
   selector: 'gifs-search-box',
   template: `
-  <h5>Buscar:</h5>
-  <input type="text"
-    class="form-control"
-    placeholder="Busar gifs..."
-    (keyup.enter)="searchTag()"
-    #txtTagInput>
+  <div class="bg-success border rounded-3">
+    <h5 class="text-light pt-2 px-2">Buscar:</h5>
+    <div class="p-2">
+      <input type="text"
+        class="form-control"
+        placeholder="Busar gifs..."
+        (keyup.enter)="searchTag()"
+        #txtTagInput>
+
+    </div>
+  </div>
   `
 })
 
